090361ee69ac9881195e44341545c463380c92f0
juliaongohh <jojuliamari@gmail.com>
2022-12-01 14:50:19 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBwnfORN8r5Hu_VF5oUXjGZKZDqXH3VqBw",
+    authDomain: "bookstore-59a3e.firebaseapp.com",
+    projectId: "bookstore-59a3e",
+    storageBucket: "bookstore-59a3e.appspot.com",
+    messagingSenderId: "723395823194",
+    appId: "1:723395823194:web:40c516b6d6bee887a77a63",
+    measurementId: "G-CPV31JN1RZ"
+  };
+  
